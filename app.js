ba110765531704b495cbec0c66f2be982360e885
PMartinGatica <pablomartin.gatica@gmail.com>
2023-05-02 14:31:37 -0300

    Formulario JS Init

--- app.js
+++ app.js
@@ -7,3 +7,44 @@ imagen.addEventListener('click', () => {
     imagenModal.src = ruta;
 });        
 });
+
+document.addEventListener("DOMContentLoaded",function(){
+    const email ={
+        nombre:"",
+        email:"",
+        asunto:"",
+        mensaje:"",
+    };
+
+    //Seleccionar elementos del HTML
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputNombre = document.querySelector('#nombre');
+    const inputMensaje = document.querySelector('#mensaje');
+
+    
+
+    // //Eventos
+    // inputEmail.addEventListener("input",validar);
+     inputAsunto.addEventListener("input",validar);
+    // inputNombre.addEventListener("input",validar);
+    // inputMensaje.addEventListener("input",validar);
+
+
+    //Funciones
+    function validar(e){
+        if (e.target.value.trim()===""){
+            mostrarAlerta();//`El campo ${e.target.id} es obligatorio`);
+            return;
+        }
+    }
+
+    function mostrarAlerta(){
+       const alerta = document.createElement('P');
+       alerta.textContent = "Hubo un error..";
+
+        formulario.appendChild(alerta);
+
+
+    }
+});
