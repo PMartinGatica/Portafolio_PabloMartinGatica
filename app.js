a61badda06c5b272a715449bb5f1af9ad4ae93bb
PMartinGatica <pablomartin.gatica@gmail.com>
2023-05-03 14:34:13 -0300

    Configuracion enviar form

--- app.js
+++ app.js
@@ -22,7 +22,36 @@ document.addEventListener("DOMContentLoaded",function(){
     const inputNombre = document.querySelector('#nombre');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-    
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const spinner = document.querySelector('#spinner');
+
+    //Configuracion para que los msj lleguen a mi casilla de mail 
+    const formularioEnviar = document.getElementById('formulario');
+
+formularioEnviar.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Obtenemos los valores de los campos del formulario
+  const nombre = document.getElementById('nombre').value;
+  const email = document.getElementById('email').value;
+  const asunto = document.getElementById('asunto').value;
+  const mensaje = document.getElementById('mensaje').value;
+
+  // Creamos el cuerpo del correo electrónico
+  const cuerpo = `Nombre: ${nombre}\nEmail: ${email}\nAsunto: ${asunto}\nMensaje: ${mensaje}`;
+
+  // Creamos el objeto de correo electrónico
+  const emailData = {
+    to: 'pablomartin.gatica@gmail.com', // Reemplaza con tu dirección de correo electrónico
+    subject: asunto,
+    body: cuerpo,
+  };
+
+  // Enviamos el correo electrónico a través de Gmail
+  GmailApp.sendEmail(emailData.to, emailData.subject, emailData.body);
+});
+
+    //Fin de configuracion
 
     // //Eventos
     inputEmail.addEventListener("blur",validar);
@@ -30,14 +59,60 @@ document.addEventListener("DOMContentLoaded",function(){
     inputNombre.addEventListener("blur",validar);
     inputMensaje.addEventListener('blur',validar);
 
+    formulario.addEventListener('submit',enviarEmail);
 
     //Funciones
+    
+    function enviarEmail(e){
+        e.preventDefault();
+        spinner.classList.add('flex');
+        spinner.classList.remove('d-none');
+        setTimeout(() => {
+            spinner.classList.add('d-none');
+            spinner.classList.remove('flex');
+
+            
+            const alertaExito= document.createElement('P');
+            alertaExito.classList.add('bg-success', 'text-white','p-2','mx-auto','fw-bold','uppercase','text-center');
+            alertaExito.textContent = 'Mensaje Enviado Correctamente';
+            btnSubmit.remove();
+            formulario.appendChild(alertaExito);
+
+
+            setTimeout(() => {
+                alertaExito.remove();
+                btnSubmit.add();
+            }, 2000);
+
+            resetFormulario();
+
+        }, 3000);
+    }
+
+    
+
     function validar(e){
         if(e.target.value.trim()===''){
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`,e.target.parentElement);//`El campo ${e.target.id} es obligatorio`);
-       return;
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`,e.target.parentElement);
+            email[e.target.name] =''
+            comprobarEmail();
+            return;
+        }
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El campo no es un email válido',e.target.parentElement);
+            email[e.target.name] =''
+            comprobarEmail();
+            return;
         }
         limpiarAlerta(e.target.parentElement);
+
+        //Asignar Valores
+
+        email[e.target.id] = e.target.value.trim().toLowerCase();
+
+        //Comprobar objeto
+
+        comprobarEmail();
     }
 
     function mostrarAlerta(mensaje,referencia){
@@ -62,5 +137,31 @@ document.addEventListener("DOMContentLoaded",function(){
         if(alerta){
             alerta.remove();
         }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);   
+        return resultado; 
+    }
+
+    function comprobarEmail(){
+   if(Object.values(email).includes('')){
+            btnSubmit.classList.add('opacity-75');
+            btnSubmit.disabled = true;
+            return
+        }
+        btnSubmit.classList.remove('opacity-75');
+        btnSubmit.disabled = false;
+    }
+
+    function resetFormulario(){
+        email.asunto='';
+        email.email = '';
+        email.mensaje= '';
+        email.nombre= '';
+
+        formulario.reset();
+         validarEmail();
     }
 });
